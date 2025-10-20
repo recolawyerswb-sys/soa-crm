dfefc0fca4ef07dd6372d5f9fb054310d0ce7e9f
Iser Solutions <iserlatam@gmail.com>
2025-10-20 14:04:42 -0500

    Fixed agents seeder 2

--- database/seeders/NewAgentsAccountsSeeder.php
+++ database/seeders/NewAgentsAccountsSeeder.php
@@ -17,25 +17,6 @@ class NewAccountsSeeder extends Seeder
      */
     public function run(): void
     {
-        // 1. Crear el usuario superadmin
-        $admin = User::create([
-            'email' => 'admin@recolawyers.com',
-            'name' => 'administrador',
-            'password' => Hash::make(':Ls3S)915")>'),
-        ]);
-
-        // Asignar rol, perfil y wallet al admin
-        $admin->assignRole('admin');
-        $admin->profile()->create([
-            'full_name' => 'Administrador',
-            'phone_1' => '31331'
-        ]);
-        $admin->wallet()->create([
-            'coin_currency' => fake()->randomElement(['USDT', 'BTC', 'ETH', 'BNB']),
-            'address' => fake()->text(20),
-            'network' => fake()->randomElement(['TRC20', 'TRX']),
-        ]);
-
         // 3. Array con la información de los nuevos agentes
         $agentes = [
             [
